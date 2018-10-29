ee50481acae95f9948fec6ed312644f13c9cb6db
DevAbas <abas.memmedov.94@gmail.com>
2018-10-29 22:08:21 +0400

    firebase dependency installed & fireabse configurations added

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBSRHmr5K9FJbM2cK2nMJKlmdOWg6WiI1Y",
+  authDomain: "nba-app-f746b.firebaseapp.com",
+  databaseURL: "https://nba-app-f746b.firebaseio.com",
+  projectId: "nba-app-f746b",
+  storageBucket: "nba-app-f746b.appspot.com",
+  messagingSenderId: "180882680122"
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseArticles = firebaseDB.ref('articles');
+const firebaseVideos = firebaseDB.ref('videos');
+const firebaseTeams = firebaseDB.ref('teams');
+
+export {
+  firebase,
+  firebaseDB,
+  firebaseArticles,
+  firebaseVideos,
+  firebaseTeams
+}
